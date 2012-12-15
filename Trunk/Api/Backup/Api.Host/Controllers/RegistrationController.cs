da2b35e8c8b5101ed70f6785ebee03acedbdc896
Kirby Bloom <esko22@gmail.com>
2012-12-15 14:20:05 -0800

    update for VS12

---
+++ Trunk/Api/Backup/Api.Host/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Amalgamate.ServicesApi.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+    }
+}
